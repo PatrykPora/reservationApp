750b80a373a3d75a1400136b443c20bb415bac4b
unknown <patryk_pora@o2.pl>
2022-03-28 21:08:13 +0200

    started with navbar and security

---
+++ src/main/java/com/reservation/reservationapp/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.reservation.reservationapp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
--- src/main/java/com/reservation/reservationapp/entity/Role.java
+++ src/main/java/com/reservation/reservationapp/entity/Role.java
@@ -2,6 +2,6 @@ package com.reservation.reservationapp.entity;
 
 public enum Role {
 
-    ROLE_REGULAR,
+    ROLE_USER,
     ROLE_ADMIN
 }

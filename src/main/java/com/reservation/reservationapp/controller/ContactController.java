9f54a75da037de5d1717f4e9551a5299455a0865
unknown <patryk_pora@o2.pl>
2022-03-30 18:47:53 +0200

    changed names of endpoints

--- src/main/java/com/reservation/reservationapp/controller/ContactController.java
+++ src/main/java/com/reservation/reservationapp/controller/ContactController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/contactpage")
+@RequestMapping("/contact")
 public class ContactController {
 
     @GetMapping
     public String getContactPage(){
-        return "contactpage";
+        return "contacts";
     }
 }
--- src/main/java/com/reservation/reservationapp/controller/ReservationController.java
+++ src/main/java/com/reservation/reservationapp/controller/ReservationController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/reservationpage")
+@RequestMapping("/reservation")
 public class ReservationController {
 
     @GetMapping
     public String getReservationPage(){
-        return "reservationpage";
+        return "reservations";
     }
 }

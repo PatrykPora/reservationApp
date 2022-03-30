3ad1d2c76bea2b0947abd5c0ef2f93c14a432999
unknown <patryk_pora@o2.pl>
2022-03-30 21:12:20 +0200

    login page and security http configure

--- src/main/java/com/reservation/reservationapp/config/SecurityConfig.java
+++ src/main/java/com/reservation/reservationapp/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.reservation.reservationapp.config;
 
 
+import com.reservation.reservationapp.entity.Role;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,8 +9,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/contact", "/contact/**",
+                        "/player", "/player/**",
+                        "/home",
+                        "/regulations",
+                        "/reservation", "/reservation/**",
+                        "/sportfacilitytermsofuse").hasRole(String.valueOf(Role.USER))
+                .antMatchers("/home").permitAll()
+                .antMatchers("/administration", "/administration/**")
+                .hasRole(String.valueOf(Role.ADMIN))
+                .antMatchers("/h2").permitAll();
+
+        http.formLogin().loginPage("/login").defaultSuccessUrl("/home", true);
+
+        http.logout().logoutSuccessUrl("/");
+
+    }
 }
--- src/main/java/com/reservation/reservationapp/entity/Role.java
+++ src/main/java/com/reservation/reservationapp/entity/Role.java
@@ -2,6 +2,6 @@ package com.reservation.reservationapp.entity;
 
 public enum Role {
 
-    ROLE_USER,
-    ROLE_ADMIN
+    USER,
+    ADMIN
 }

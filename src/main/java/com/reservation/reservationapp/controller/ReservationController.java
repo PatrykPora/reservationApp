b5a529dae85cffd0203ada27937d74687c506317
unknown <patryk_pora@o2.pl>
2022-04-11 18:56:10 +0200

    reservation controller template

--- src/main/java/com/reservation/reservationapp/controller/ReservationController.java
+++ src/main/java/com/reservation/reservationapp/controller/ReservationController.java
@@ -4,6 +4,7 @@ import com.reservation.reservationapp.dto.ReservationDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +14,15 @@ public class ReservationController {
     @GetMapping
     public String getReservationPage() {
         return "reservations";
+    }
+
+    @GetMapping("/create")
+    public String getReservationForm(){
+        return "reservationForm";
+    }
+
+    @PostMapping("/create")
+    public String createReservation(){
+        return "home";
     }
 }
